ca542d81c6b56c9c15301e03d18cc6bdcf3a15ce
Damiano Mauro <d.mauro@teamsystem.com>
2023-09-21 17:34:05 +0200

    Add project files.

---
+++ Home.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace JSONEditor
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtJson = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(11, 375);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 33;
+            this.dataGridView.Size = new System.Drawing.Size(1219, 498);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // txtJson
+            // 
+            this.txtJson.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJson.Location = new System.Drawing.Point(11, 12);
+            this.txtJson.Multiline = true;
+            this.txtJson.Name = "txtJson";
+            this.txtJson.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJson.Size = new System.Drawing.Size(1218, 311);
+            this.txtJson.TabIndex = 5;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(11, 328);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(111, 33);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Convert";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Modifica";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 887);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtJson);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtJson;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Home.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using DataTable = System.Data.DataTable;
+
+namespace JSONEditor
+{
+    public partial class Home : Form
+    {
+        private DataTable dataTable;
+        private string jsonString;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (!Check())
+                return;
+
+            string jsonString = txtJson.Text;
+
+            try
+            {
+                DataTable dataTable = JsonConvert.DeserializeObject<DataTable>(jsonString);
+                dataGridView.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore durante il caricamento del JSON: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Boolean Check()
+        {
+            var result = true;
+
+            if (txtJson.Text.Trim() == "")
+                result = false;
+
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Check())
+                return;
+
+            try
+            {
+                dataTable = (DataTable)dataGridView.DataSource;
+
+                jsonString = JsonConvert.SerializeObject(dataTable, Formatting.Indented);
+
+                if (!jsonString.StartsWith("[") || !jsonString.EndsWith("]"))
+                    jsonString = "[" + jsonString + "]";
+
+                // Scrivi la stringa JSON nel file di testo
+                //File.WriteAllText("data.json", jsonString);
+                //MessageBox.Show("Modifiche salvate correttamente.", "Salvataggio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtJson.Text = jsonString;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore durante il salvataggio del JSON: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+            dataGridView.DataSource = dataTable;
+        }
+
+        void CleanAllForm(){
+            txtJson.Text = "";
+            dataGridView.DataSource = null;
+        }
+    }
+}
